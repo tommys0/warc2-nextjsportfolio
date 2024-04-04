1f6a1b00c480f18d868ae96734c1ba9970fccb67
tommys0 <149568540+tommys0@users.noreply.github.com>
2024-04-04 10:40:32 +0200

    Two boxes section 2

--- my-portfolio/src/app/page.js
+++ my-portfolio/src/app/page.js
@@ -1,9 +1,10 @@
 import './globals.css';
 import Image from "next/image";
+import Card from './components/card';
 
 export default function Page() {
     return (
-        <main className="flex justify-center mt-10">
+        <main className="flex flex-col items-center justify-center mt-10">
             {/* Box part */}
             <div className={'w-10/12 flex flex-col md:flex-row gap-2 font-light'}>
                 {/* Left side*/}
@@ -28,10 +29,9 @@ export default function Page() {
                                 sophisticated design principles.</p>
                         </div>
                     </div>
-
                 </div>
                 {/* Right side */}
-                <div className={'flex-1 flex justify-center'}>
+                <div className={'flex-1 flex justify-center relative'}>
                     <div className={'max-w-full relative'}>
                         <Image src={'/gray-house.webp'} alt={'gray house'} layout={'responsive'} width={300}
                                height={300} className={'rounded-3xl'}/>
@@ -46,6 +46,23 @@ export default function Page() {
                     </div>
                 </div>
             </div>
+            {/* Full width 2 boxes */}
+            <div className='w-full bg-customLighterGray mt-10'>
+                <div className='w-10/12 flex justify-between mx-auto my-8 gap-2'>
+                    <div className='bg-customGray w-5/12 p-5 rounded-3xl text-white'>
+                        <p className='font-light mb-40 text-xl'>LEARN HOW WE THINK</p>
+                        <p className='text-4xl'>LUDWIG MIES VAN DER ROHE</p>
+                    </div>
+                    <div className='border relative border-solid border-black w-full text-black p-5 rounded-3xl'>
+                        <button className='flex items-center mt-2 px-3 py-1 bg-white text-black font-light rounded-full'>
+                            ABOUT US
+                            <Image src={'/arrow.png'} alt={'arrow'} width={'20'} height={'20'}/>
+                        </button>
+                        <p className='absolute bottom-5 left-5 mr-3 text-4xl font-light'>¨A CHAIR IS A VERY DIFFICULT OBJECT. A SKYSCRAPER IS ALMOST EASIER. THAT IS WHY CHIPPENDALE IS FAMOUS.¨</p>
+                    </div>
+                </div>
+            </div>
+            
         </main>
     )
 }
