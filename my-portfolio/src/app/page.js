632ff8bcb787a28267ec2aa9285ec47e452d2f72
tommys0 <149568540+tommys0@users.noreply.github.com>
2024-04-04 23:15:34 +0200

    Added Card Part

--- my-portfolio/src/app/page.js
+++ my-portfolio/src/app/page.js
@@ -62,7 +62,37 @@ export default function Page() {
                     </div>
                 </div>
             </div>
-            
+            {/* Card section */}
+            <div className='flex gap-20 my-20 font-light mx-20 justify-between'>
+                <Card
+                    icon={
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30">
+                            <path d="M15 9H9v6h6V9zm-2 4h-2v-2h2v2zm8-2V9h-2V7c0-1.1-.9-2-2-2h-2V3h-2v2h-2V3H9v2H7c-1.1 0-2 .9-2 2v2H3v2h2v2H3v2h2v2c0 1.1.9 2 2 2h2v2h2v-2h2v2h2v-2h2c1.1 0 2-.9 2-2v-2h2v-2h-2v-2h2zm-4 6H7V7h10v10z"/>
+                        </svg>
+                    }
+                    name={'ADAPTABILITY'}
+                    description={'Embracing change, our designs evolve with time, ensuring relevance and functionality in every architectural endeavor.'}
+                ></Card>
+                <Card
+                    icon={
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30">
+                            <path d="M12 2L4.5 20.29l.71.71L12 18l6.79 3 .71-.71z"/>
+                        </svg>
+                    }
+                    name={'QUALITY'}
+                    description={'Crafted with precision, our commitment to quality transcends aesthetics, defining every detail of our architectural creations.'}
+                ></Card>
+                <Card
+                    icon={
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30">
+                            <path d="M1 11v10h5v-6h4v6h5V11L8 6z"/><path d="M10 3v1.97l7 5V11h2v2h-2v2h2v2h-2v4h6V3H10zm9 6h-2V7h2v2z"/>
+                    </svg>
+                    }
+                    name={'CLIENT-CENTRIC'}
+                    description={'Placing your vision at the forefront, we design spaces that not only meet expectations but exceed them, making your dreams a reality.'}
+                ></Card>
+            </div>
+            {/* Recent Project*/}
         </main>
     )
 }
