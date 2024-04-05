8969bf5ef17921a76ec325cdbb61d9b3e6a1ebfc
tommys0 <149568540+tommys0@users.noreply.github.com>
2024-04-05 14:31:07 +0200

    Icon Color Invert

--- my-portfolio/src/app/components/card.js
+++ my-portfolio/src/app/components/card.js
@@ -5,7 +5,7 @@ import Image from 'next/image';
 const Card = ({ icon, name, description }) => {
   return (
     <Link href="/path/to/destination">
-      <div className='p-5 relative border border-solid rounded-lg border-white hover:border-black transition-colors duration-300 ease-in-out block cursor-pointer'>
+      <div className='p-5 relative border border-solid rounded-3xl border-white hover:border-black transition-colors duration-300 ease-in-out block cursor-default'>
         {/* ICON */}
         <div>{icon}</div>
         {/* NAME */}
@@ -13,9 +13,9 @@ const Card = ({ icon, name, description }) => {
         {/* DESCRIPTION */}
         <p className='mb-10 flex relative text-'>{description}</p>
         {/* BUTTON */}
-        <button className='flex absolute bottom-5 left-5 items-center mt-2 px-3 py-1 bg-customDarkGray text-white font-light rounded-full'>
+        <button className='flex absolute bottom-5 left-5 items-center mt-2 px-3 py-1 bg-customDarkGray text-white font-light rounded-full hover:cursor-pointer'>
           Learn More
-          <Image src={'/arrow.png'} alt={'arrow'} width={'20'} height={'20'}/>
+          <Image src={'/arrow.png'} alt={'arrow'} width={'20'} height={'20'} className='invert'/>
         </button>
       </div>
     </Link>
