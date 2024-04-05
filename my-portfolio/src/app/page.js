b93b2d34027f74812b596ef207d55b5070ca8ac8
tommys0 <149568540+tommys0@users.noreply.github.com>
2024-04-05 12:35:34 +0200

    Recent Projects Test - not working

--- my-portfolio/src/app/page.js
+++ my-portfolio/src/app/page.js
@@ -1,6 +1,7 @@
 import './globals.css';
 import Image from "next/image";
 import Card from './components/card';
+import infoCard from './components/infoCard';
 
 export default function Page() {
     return (
@@ -34,13 +35,13 @@ export default function Page() {
                 <div className={'flex-1 flex justify-center relative'}>
                     <div className={'max-w-full relative'}>
                         <Image src={'/gray-house.webp'} alt={'gray house'} layout={'responsive'} width={300}
-                               height={300} className={'rounded-3xl'}/>
+                            height={300} className={'rounded-3xl'} />
                         <div
                             className={'absolute border border-solid border-white text-white bottom-2 rounded-3xl p-5 mx-2'}>
                             <p className="text-3xl mb-10">IT IS NOT POSSIBLE TO GO FORWARD WHILE LOOKING BACK.</p>
                             <button className="flex items-center mt-2 px-3 py-1 bg-white text-black rounded-full">
                                 BOOK A CALL
-                                <Image src={'/arrow.png'} alt={'arrow'} width={'20'} height={'20'}/>
+                                <Image src={'/arrow.png'} alt={'arrow'} width={'20'} height={'20'} />
                             </button>
                         </div>
                     </div>
@@ -56,18 +57,18 @@ export default function Page() {
                     <div className='border relative border-solid border-black w-full text-black p-5 rounded-3xl'>
                         <button className='flex items-center mt-2 px-3 py-1 bg-white text-black font-light rounded-full'>
                             ABOUT US
-                            <Image src={'/arrow.png'} alt={'arrow'} width={'20'} height={'20'}/>
+                            <Image src={'/arrow.png'} alt={'arrow'} width={'20'} height={'20'} />
                         </button>
                         <p className='absolute bottom-5 left-5 mr-3 text-4xl font-light'>¨A CHAIR IS A VERY DIFFICULT OBJECT. A SKYSCRAPER IS ALMOST EASIER. THAT IS WHY CHIPPENDALE IS FAMOUS.¨</p>
                     </div>
                 </div>
             </div>
             {/* Card section */}
-            <div className='flex gap-20 my-20 font-light mx-20 justify-between'>
+            <div className='flex gap-2 my-20 font-light mx-20 justify-between w-10/12'>
                 <Card
                     icon={
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30">
-                            <path d="M15 9H9v6h6V9zm-2 4h-2v-2h2v2zm8-2V9h-2V7c0-1.1-.9-2-2-2h-2V3h-2v2h-2V3H9v2H7c-1.1 0-2 .9-2 2v2H3v2h2v2H3v2h2v2c0 1.1.9 2 2 2h2v2h2v-2h2v2h2v-2h2c1.1 0 2-.9 2-2v-2h2v-2h-2v-2h2zm-4 6H7V7h10v10z"/>
+                            <path d="M15 9H9v6h6V9zm-2 4h-2v-2h2v2zm8-2V9h-2V7c0-1.1-.9-2-2-2h-2V3h-2v2h-2V3H9v2H7c-1.1 0-2 .9-2 2v2H3v2h2v2H3v2h2v2c0 1.1.9 2 2 2h2v2h2v-2h2v2h2v-2h2c1.1 0 2-.9 2-2v-2h2v-2h-2v-2h2zm-4 6H7V7h10v10z" />
                         </svg>
                     }
                     name={'ADAPTABILITY'}
@@ -76,7 +77,7 @@ export default function Page() {
                 <Card
                     icon={
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30">
-                            <path d="M12 2L4.5 20.29l.71.71L12 18l6.79 3 .71-.71z"/>
+                            <path d="M12 2L4.5 20.29l.71.71L12 18l6.79 3 .71-.71z" />
                         </svg>
                     }
                     name={'QUALITY'}
@@ -85,14 +86,37 @@ export default function Page() {
                 <Card
                     icon={
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="30" height="30">
-                            <path d="M1 11v10h5v-6h4v6h5V11L8 6z"/><path d="M10 3v1.97l7 5V11h2v2h-2v2h2v2h-2v4h6V3H10zm9 6h-2V7h2v2z"/>
-                    </svg>
+                            <path d="M1 11v10h5v-6h4v6h5V11L8 6z" /><path d="M10 3v1.97l7 5V11h2v2h-2v2h2v2h-2v4h6V3H10zm9 6h-2V7h2v2z" />
+                        </svg>
                     }
                     name={'CLIENT-CENTRIC'}
                     description={'Placing your vision at the forefront, we design spaces that not only meet expectations but exceed them, making your dreams a reality.'}
                 ></Card>
             </div>
             {/* Recent Project*/}
+            <div className='bg-customLightGray w-full text-center'>
+                <div className='w-10/12 mx-auto'>
+                    <div className='flex justify-between my-10'>
+                        <p className='text-4xl'>RECENT PROJECTS</p>
+                        <button className='flex items-center mt-2 px-3 py-1 bg-customDarkGray text-white font-light rounded-full hover:cursor-pointer'>
+                            All Projects
+                            <Image src={'/arrow.png'} alt={'arrow'} width={'20'} height={'20'} className='invert' />
+                        </button>
+                    </div>
+                    <div className=''>
+                        {/* CARD 1 */}
+                        <div className=''>
+                            <infoCard
+                                name='INNOVATIVE BALANCE'
+                                date='JAN 26, 2024'
+                                city='BERLIN'
+                                service='COMMERCIAL DESIGN'
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </main>
     )
 }
