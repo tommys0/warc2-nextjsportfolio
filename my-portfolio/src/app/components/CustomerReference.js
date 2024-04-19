9a8944af5a1c23c0a6e79cbe848e76fdc07d9e50
tommys0 <149568540+tommys0@users.noreply.github.com>
2024-04-19 22:58:04 +0200

    CustomerReference: Basic component structure

--- my-portfolio/src/app/components/CustomerReference.js
+++ my-portfolio/src/app/components/CustomerReference.js
@@ -1,7 +1,13 @@
+import Image from "next/image";
+
 export default function CustomerReference({ num, referenceText, pfp, name, position }) {
     return (
         <div>
-
+            {/* SVG 473 NEEDED*/}
+            <p>{referenceText}</p>
+            <Image src={pfp} alt={'profile picture'} width={50} height={50}></Image>
+            <p>{name}</p>
+            <p>{position}</p>
         </div>
     );
 }
