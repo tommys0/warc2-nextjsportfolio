48496ab39d6f86714b53453a8ba3b5fc7eb7b97c
tommys0 <149568540+tommys0@users.noreply.github.com>
2024-04-21 23:09:20 +0200

    Final design REFERENCE CARDS

--- my-portfolio/src/app/components/CustomerReference.js
+++ my-portfolio/src/app/components/CustomerReference.js
@@ -1,13 +1,28 @@
 import Image from "next/image";
 
-export default function CustomerReference({ num, referenceText, pfp, name, position }) {
+export default function CustomerReference({ referenceText, pfp, name, position }) {
     return (
-        <div>
-            {/* SVG 473 NEEDED*/}
-            <p>{referenceText}</p>
-            <Image src={pfp} alt={'profile picture'} width={50} height={50}></Image>
-            <p>{name}</p>
-            <p>{position}</p>
+        <div className="p-5 relative border border-solid rounded-3xl border-white hover:border-black transition-colors duration-300 ease-in-out h-[400px] overflow-hidden w-[300px] flex flex-col justify-between">
+                <p className="text-5xl font-bold">473.</p>
+
+            <div className="flex-grow text-left text-customGray my-5 overflow-auto">
+                <p>{referenceText}</p>
+            </div>
+
+            <div className="flex flex-col">
+                <div className="rounded-full overflow-hidden w-12 h-12">
+                    <Image
+                        src={pfp}
+                        alt="Profile picture"
+                        width={48}
+                        height={48}
+                        layout="responsive"
+                        className="rounded-full"
+                    />
+                </div>
+                <p className={'my-1'}>{name}</p>
+                <p className="text-sm text-customGray font-light">{position}</p>
+            </div>
         </div>
     );
 }
